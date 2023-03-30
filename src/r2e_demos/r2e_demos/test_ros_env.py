d3b1aac1c68bf4ff3fbf25cde44c8c0000a471df
k4t-hs <alka1020@h-ka.de>
2023-03-30 22:33:54 +0200

    changed poses to safer locations in cell

--- src/r2e_demos/r2e_demos/test_ros_env.py
+++ src/r2e_demos/r2e_demos/test_ros_env.py
@@ -8,13 +8,10 @@ def main(args=None):
     rclpy.init(args=args)
     scene = RosScene(is_simulation=True)
     
-    ramp_bottom_pose = Affine((0.0, -0.45, 1.1), (0.0, np.radians(180+45), 0.0))
-    ramp_top_pose = Affine((-0.2, -0.45, 1.25), (0.0, 1.0, 0.0, 0.0))
-    
     scene.robot.home()
-    scene.robot.ptp(ramp_bottom_pose)
-    scene.robot.ptp(ramp_top_pose)
-    scene.robot.lin(Affine((0, -0.45, 1.2), (0.0, 1.0, 0.0, 0.0)))
+    scene.robot.ptp(Affine((0.0, -0.2, 1.2), (0.0, np.radians(180+45), 0.0)))
+    scene.robot.ptp(Affine((-0.2, 0.3, 1.25), (0.0, 1.0, 0.0, 0.0)))
+    scene.robot.lin(Affine((0.1, 0.0, 1.2), (0.0, 1.0, 0.0, 0.0)))
     scene.robot.open_gripper()
     scene.robot.close_gripper()
     
