31c94f6e050bd5ecc4bf966e886af374990f145e
Gergely <gergely.soti@h-ka.de>
2023-03-31 13:55:48 +0200

    changed RosScene to RobotClient

--- src/r2e_demos/r2e_demos/test_ros_env.py
+++ src/r2e_demos/r2e_demos/test_ros_env.py
@@ -1,21 +1,21 @@
 # -*- coding: utf-8 -*-
 import rclpy
 import numpy as np
-from ros_environment.scene import RosScene
+from ros_environment.scene import RobotClient
 from ros_environment.transform import Affine
 
 def main(args=None):
     rclpy.init(args=args)
-    scene = RosScene(is_simulation=True)
+    robot = RobotClient(is_simulation=True)
     
-    scene.robot.home()
-    scene.robot.ptp(Affine((0.0, -0.2, 1.2), (0.0, np.radians(180+45), 0.0)))
-    scene.robot.ptp(Affine((-0.2, 0.3, 1.25), (0.0, 1.0, 0.0, 0.0)))
-    scene.robot.lin(Affine((0.1, 0.0, 1.2), (0.0, 1.0, 0.0, 0.0)))
-    scene.robot.open_gripper()
-    scene.robot.close_gripper()
-    
-    scene.shutdown()
+    robot.home()
+    robot.ptp(Affine((0.0, -0.2, 1.2), (0.0, np.radians(180+45), 0.0)))
+    robot.ptp(Affine((-0.2, 0.3, 1.25), (0.0, 1.0, 0.0, 0.0)))
+    robot.lin(Affine((0.1, 0.0, 1.2), (0.0, 1.0, 0.0, 0.0)))
+    robot.open_gripper()
+    robot.close_gripper()
+
+    robot.destroy_node()
     rclpy.shutdown()
 
 
