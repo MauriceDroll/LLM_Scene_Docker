fb27429f0fabaae256e59f05beedd8a4debf4867
IRAS-IRL3-LIN <alka1020@h-ka.de>
2023-04-03 19:59:54 +0200

    new poses from real cell, new pre-poses, new matrix calculation

--- src/r2e_demos/r2e_demos/r2e_cubes.py
+++ src/r2e_demos/r2e_demos/r2e_cubes.py
@@ -11,36 +11,34 @@ class CubesDemo():
             self,
             is_simulation: bool = False,
             home_pose: Affine = Affine(
-                (-0.046, -0.413, 1.070), 
-                (0.000, 0.907, -0.000, 0.420)),
-            ramp_top: Affine = Affine(
-                (-0.189, -0.413, 1.179), 
-                (0.000, 0.706, -0.001, 0.708)),
-            ramp_bottom: Affine = Affine(
-                (-0.046, -0.413, 1.070), 
-                (0.000, 0.907, -0.000, 0.420))) -> None:
+                (0.122, -0.052, 1.426), 
+                (0.004, 0.860, 0.007, 0.510)),
+            ramp_bottom=Affine(
+                (-0.114, -0.484, 1.050), 
+                (0.009, 0.932, 0.001, -0.362)),
+            ramp_top=Affine(
+                (-0.250, -0.484, 1.193), 
+                (0.009, 0.932, 0.001, -0.362))) -> None:
         """ Initialize the cubes demo class.
 
         Parameters
         ----------
         is_simulation : bool, default=False
             Defines if the demo runs on the real robot or in simulation.
-        home_pose : Affine, default=Affine((0.015, -0.053, 1.458), (0.006, 0.707, 0.006, 0.707))
+        home_pose : Affine, default=Affine((0.122, -0.052, 1.426), (0.004, 0.860, 0.007, 0.510)
             The robot's cartesian home pose.
-        ramp_top : Affine, default=Affine((-0.204, -0.483, 1.287), (0.009, 0.994, -0.001, -0.112))
+        ramp_top : Affine, default=Affine((-0.250, -0.484, 1.193), (0.009, 0.932, 0.001, -0.362))
             The robot's cartesian pose to reach the top of the cubes ramp. The default pose
             is correct if the ramp is positioned as follows: the mounting holes that are
             nearer to the ramp respectively are used. The mounting holes are at positions
             B13 (further away from ramp) and B9 (directly next to ramp).
             ramp-side (hole B9) (unused hole) space (hole B13) (unused hole) vertical-column-side
-        ramp_bottom : Affine, default=Affine((-0.062, -0.483, 1.149), (0.009, 0.0994, -0.001, -0.112))
+        ramp_bottom : Affine, default=Affine((-0.114, -0.484, 1.050), (0.009, 0.932, 0.001, -0.362))
             The robots' cartesian pose to reach the cubes at the bottom of the ramp.
             The default pose is correct if the ramp is positioned as described in "ramp_top"
             parameter description.
 
         """
-        # TODO change all 3 default poses
-        # TODO set home_pose in RobotClient after changed in ros_env robot
         self.robot = RobotClient(is_simulation=is_simulation)
         self.robot.home_pose=home_pose
         self.ramp_top = ramp_top
@@ -49,23 +47,24 @@ class CubesDemo():
     def run_demo(
             self, 
             matrix_center: Affine = Affine(
-                (0.114, -0.152, 1.137), 
-                (0.008, 0.966, 0.002, 0.259)),
-            matrix_distance: float = 0.074) -> None:
+                (0.123, -0.024, 1.026), 
+                (0.008, 1.000, 0.004, 0.013)),
+            distance_x: float = 0.04,
+            distance_y: float = 0.06) -> None:
         """ Run the cubes demo: build a 3x3 matrix of cubes and tidy up afterwards.
         
         Parameters
         ----------
-        matrix_center : Affine, default=Affine((0.1, -0.052, 1.15), (0.0, 1.0, 0.0, 0.0))
+        matrix_center : Affine, default=Affine((0.123, -0.024, 1.026), (0.008, 1.000, 0.004, 0.013))
             The pose to position the central cube of the matrix.
-        distance : float, default=0.074
+        matrix_distance : float, default=0.074
             The distance (in meters) between the cube centers in the matrix. Be careful 
             to choose a value that leaves enough space for the opened gripper.
 
         """
         # TODO change matrix center default pose
         self.robot.home()
-        matrix = self.get_matrix_poses(matrix_center, matrix_distance)
+        matrix = self.get_matrix_poses(matrix_center, distance_x=distance_x, distance_y=distance_y)
         self.set_up_cubes(matrix)
         self.robot.home()
         self.tidy_up_cubes(matrix)
@@ -79,7 +78,7 @@ class CubesDemo():
         self.robot.destroy_node()
         
     @staticmethod
-    def get_matrix_poses(center_pose: Affine, distance: float) -> List[Affine]:
+    def get_matrix_poses(center_pose: Affine, distance_x: float, distance_y: float) -> List[Affine]:
         """ Get each pose for the 3x3 cube matrix.
 
         Parameters
@@ -95,13 +94,12 @@ class CubesDemo():
             List of the cube matrix poses.
 
         """
-
         poses = list()
-        for x_direction in [-1, 0, 1]:
-            for y_direction in [-1, 0, 1]:
+        for x_direction_dist in [-distance_x, 0, distance_x]:
+            for y_direction_dist in [-distance_y, 0, distance_y]:
                 copy_pose = Affine(center_pose.translation, center_pose.quat)
-                copy_pose.translation[0] = copy_pose.translation[0] + x_direction * distance
-                copy_pose.translation[1] = copy_pose.translation[1] + y_direction * distance
+                copy_pose.translation[0] = copy_pose.translation[0] + x_direction_dist
+                copy_pose.translation[1] = copy_pose.translation[1] + y_direction_dist
                 poses.append(copy_pose)
         return poses
     
@@ -122,9 +120,8 @@ class CubesDemo():
             The pre-pose (pre-pick/pre-place pose).
 
         """
-        # TODO change to correct offsets
-        translation = (pose.translation[0], pose.translation[1], pose.translation[2]+distance)
-        pre_pose = Affine(translation, (pose.quat))
+        pre_pose_transform = Affine(translation=(0, 0, -distance))
+        pre_pose = pose * pre_pose_transform
         return pre_pose
     
     def pick(self, pick_pose: Affine) -> None:
@@ -198,28 +195,29 @@ def main(args=None):
      are used. The mounting holes are fixed at positions B9 (directly next to 
      ramp) and B13 (further away from ramp).
     --> ramp-side (hole B9) (unused hole) space (hole B13) (unused hole) vertical-column-side
-    
     To use the ramp at another position, please change the ramp_top and 
      ramp_bottom parameters when initialising the CubesDemo object.
-      
+
+    To follow the KUKA cubes tutorial worksheet please position the frame holes 
+     at H5, H1, N5, N1.
     The cube matrix position can be changed as well if desired by setting the
      matrix_center and matrix_distance parameters of the run_demo function as
      described in its docstring.
+
+    To test the demo on the real robot with opening and closing the gripper, please set
+     is_simulation=False when initialising the CubesDemo.
     """
     # initialise the cubes demo
-    # TODO remove incorrect poses / change to new default poses
     cubes = CubesDemo(
-        is_simulation=True,
-        home_pose=Affine((0.206, -0.049, 1.340), (0.000, 0.407, -0.000, 0.914)),
-        ramp_bottom=Affine((-0.046, -0.413, 1.070), (0.000, 0.907, -0.000, 0.420)),
-        ramp_top=Affine((-0.189, -0.413, 1.179), (0.000, 0.706, -0.001, 0.708)))
-    # run the cubes demo; the position of the cube matrix can be changed by
-    #  setting the matrix_center and matrix_distance parameters as described
-    #  in the rund_demo function's docstring
-    # TODO remove matrix center and distance / change to new default
+        is_simulation=False,
+        home_pose=Affine((0.122, -0.052, 1.426), (0.004, 0.860, 0.007, 0.510)),
+        ramp_bottom=Affine((-0.114, -0.484, 1.050), (0.009, 0.932, 0.001, -0.362)),
+        ramp_top=Affine((-0.250, -0.484, 1.193), (0.009, 0.932, 0.001, -0.362)))
+    # run the cubes demo
     cubes.run_demo(
-        matrix_center=Affine((0.136, -0.049, 1.033), (0.000, 0.706, -0.000, 0.708)),
-        matrix_distance=0.074)
+        matrix_center=Affine((0.123, -0.024, 1.026), (0.008, 1.000, 0.004, 0.013)),
+        distance_x=0.04,
+        distance_y=0.06)
     
     # shutdown previously initialized context
     rclpy.shutdown()
