2cd6802a17de0002d604ec617f6767d0f9086c46
IRAS-IRL3-LIN <alka1020@h-ka.de>
2023-04-20 15:10:15 +0200

    new poses and affine transformations

--- src/r2e_demos/r2e_demos/test_ros_env.py
+++ src/r2e_demos/r2e_demos/test_ros_env.py
@@ -1,6 +1,7 @@
 import rclpy
 from ros_environment.scene import RobotClient
 from ros_environment.transform import Affine
+import time
 
 def main(args=None):
     # initialize ros communications for a given context
@@ -8,24 +9,38 @@ def main(args=None):
     
     # initialize robot client node; to open or close the gripper of the real 
     #  robot set is_simulation=False
-    robot = RobotClient(is_simulation=True)
+    robot = RobotClient(is_simulation=False)
+    robot.home_pose = Affine((0.122, -0.052, 1.426), (0.004, 0.860, 0.007, 0.510))
     
     # move robot to home pose
     robot.home()
     # robot ptp movement to given pose
     #  first tuple represents cartesian coordinates (x, y, z), the second tuple 
     #  represents rotation in quaternions (x, y, z, w)
-    robot.ptp(Affine((-0.075, -0.171, 1.145), (0.000, 0.710, -0.000, 0.704)))
+    robot.ptp(Affine((0.048, -0.308, 1.120), (0.000, 0.946, -0.000, -0.324)))
     # open gripper (does nothing when only in simulation)
     robot.open_gripper()
     # robot ptp movement again
-    robot.ptp(Affine((0.043, 0.144, 1.270), (-0.000, -0.092, -0.000, 0.996)))
+    robot.ptp(Affine((-0.151, 0.243, 1.111), (0.666, 0.746, 0.014, 0.016)))
     # robot lin movement to given pose
-    robot.lin(Affine((0.000, -0.052, 1.088), (-0.000, 0.704, 0.000, 0.711)))
+    robot.lin(Affine((-0.051, 0.243, 1.111), (0.666, 0.746, 0.014, 0.016)))
     # close gripper (does nothing when only in simulation)
     robot.close_gripper()
     # back to home pose
     robot.home()
+
+    # translation of the tcp
+    # translation in tcp coordinate system (-0.05 m in z_tcp direction)
+    movement_tcp = Affine((0, 0, -0.05))
+    # apply translation to current pose (home) given in world coordinates
+    pose = robot.home_pose * movement_tcp
+    robot.lin(pose)
+
+    # translation in world coordinate system (0.1 in z_world direction)
+    movement_world = Affine((0, 0, 0.1))
+    # apply translation to current pose given in world coordinates
+    pose = movement_world * pose
+    robot.lin(pose)
 
     # destroy the robot node
     robot.destroy_node()
