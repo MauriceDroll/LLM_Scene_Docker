b95da0fd7c12c0ed19fad89c9276249fd196e27a
Gergely <gergely.soti@h-ka.de>
2024-04-02 17:55:54 +0200

    added return success to robot client calls

--- src/r2e_tutorial/r2e_tutorial/example.py
+++ src/r2e_tutorial/r2e_tutorial/example.py
@@ -27,31 +27,28 @@ def main(args=None):
     current_pose = robot.node.get_transform('tcp_link', 'world')
     print(current_pose)
     # robot ptp movement to given joint positions
-    print('ptp start')
-    robot.ptp_joint([np.pi / 4, -np.pi / 2, np.pi / 2, 0.0, -np.pi / 4, 0.0])
-    print('ptp done')
+    success = robot.ptp_joint([np.pi / 4, -np.pi / 2, np.pi / 2, 0.0, -np.pi / 4, 0.0])
+    print('ptp success', success)
     time.sleep(2)
     # robot ptp movement to given cartesian pose
     #  first tuple represents cartesian coordinates (x, y, z), the second tuple
     #  represents rotation in quaternions (x, y, z, w)
-    print('ptp start')
-    robot.ptp(Affine((0.03607227, 0.15249834, 1.20871037),
-                     (3.80250600e-05, 9.89829634e-01, 4.76747912e-05, 1.42257839e-01)))
-    print('ptp done')
+    success = robot.ptp(Affine((0.03607227, 0.15249834, 1.20871037),
+                               (3.80250600e-05, 9.89829634e-01, 4.76747912e-05, 1.42257839e-01)))
+    print('ptp success', success)
     time.sleep(2)
     current_pose = robot.node.get_transform('tcp_link', 'world')
     print(current_pose)
     # open gripper (does nothing in simulation)
-    robot.open_gripper()
+    success = robot.open_gripper()
+    print('open gripper success', success)
     # robot ptp movement again; you can also use RPY angles instead of quaternions
-    print('ptp start')
-    robot.ptp(Affine((-0.151, 0.193, 1.111), (np.pi, 0, -np.pi / 2)))
-    print('ptp done')
+    success = robot.ptp(Affine((-0.151, 0.193, 1.111), (np.pi, 0, -np.pi / 2)))
+    print('ptp success', success)
     time.sleep(2)
     # robot lin movement to given pose
-    print('lin start')
-    robot.lin(Affine((-0.051, 0.193, 1.111), (np.pi, 0, -np.pi / 2)))
-    print('lin done')
+    success = robot.lin(Affine((-0.051, 0.193, 1.111), (np.pi, 0, -np.pi / 2)))
+    print('lin success', success)
     time.sleep(2)
     # close gripper (does nothing when in simulation)
     robot.close_gripper()
@@ -64,23 +61,26 @@ def main(args=None):
     # apply translation to current pose (home) given in world coordinates
     current_pose = robot.node.get_transform('tcp_link', 'world')
     pose = current_pose * movement_tcp
-    print('lin start')
-    robot.lin(pose)
-    print('lin done')
+    success = robot.lin(pose)
+    print('lin success', success)
     time.sleep(2)
 
     # translation in world coordinate system (0.1 in z_world direction)
     movement_world = Affine((0, 0, 0.1))
     # apply translation to current pose given in world coordinates
     pose = movement_world * pose
-    print('lin start')
-    robot.lin(pose)
-    print('lin done')
+    success = robot.lin(pose)
+    print('lin success', success)
     time.sleep(2)
+
+    movement_world = Affine((0, 0, 0.1))
+    pose = movement_world * pose
+    # the method returns False if the movement was not successful
+    success = robot.ptp(pose)
+    print('ptp success', success)
 
     # you can define more complex movements using methods
     current_pose = robot.node.get_transform('tcp_link', 'world')
-    print('complex movement start')
     complex_movement_example(robot, current_pose)
     print('complex movement done')
     time.sleep(2)
