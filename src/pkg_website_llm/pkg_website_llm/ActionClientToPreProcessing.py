252d4d9be35b7298f09e3632236a8117aadf2e8b
Maurice Droll <drma1025@h-ka.de>
2024-07-23 15:59:42 +0200

    Added Action Client for Communication between website and llm

---
+++ src/pkg_website_llm/pkg_website_llm/ActionClientToPreProcessing.py
@@ -0,0 +1,55 @@
+import rclpy
+from rclpy.action import ActionClient
+from rclpy.node import Node
+
+from llm_action_interfaces.action import LLM
+
+
+class LLMActionClient(Node):
+
+    def __init__(self):
+        super().__init__('llm_action_client')
+        self._action_client = ActionClient(self, LLM, 'website_llm_client')
+
+    def send_goal(self, user_input):
+        request_msg = LLM.Goal()
+        request_msg.userinput = str(user_input)
+
+        self._action_client.wait_for_server()
+
+        self._send_goal_future = self._action_client.send_goal_async(request_msg)
+        
+        self._send_goal_future.add_done_callback(self.goal_response_callback)
+        
+    
+    def goal_response_callback(self, future):
+        
+        goal_handle = future.result()
+        
+        if not goal_handle.accepted:
+            self.get_logger().info('LLM request rejected :(')
+            return
+
+        self.get_logger().info('LLM request accepted :)')
+
+        self._get_result_future = goal_handle.get_result_async()
+        self._get_result_future.add_done_callback(self.get_result_callback)
+        
+
+    def get_result_callback(self, future):
+        result = future.result().result
+        self.get_logger().info('Result: {0}'.format(result.sequence))
+        rclpy.shutdown()
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    action_client = LLMActionClient()
+
+    future = action_client.send_goal(10)
+
+    rclpy.spin_until_future_complete(action_client, future)
+
+
+if __name__ == '__main__':
+    main()
